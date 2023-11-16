cec0913866f5cac5b58e1075ec21da58e35e76c4
alexandraciobanuey <alexandra.ciobanu@ro.ey.com>
2023-11-16 12:44:13 +0200

    add style.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import '.assets/styles/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,4 +12,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
