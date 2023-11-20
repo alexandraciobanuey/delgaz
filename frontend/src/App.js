c3a8728b4e0cc9522ed537c3ba12e9caaf3c6ac9
alexandraciobanuey <alexandra.ciobanu@ro.ey.com>
2023-11-20 13:30:44 +0200

    install router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     <Header/>
     <main className="py-3">
       <Container>
-        <h1>Bine ai venit la Delgaz!2</h1>
+        <h1>Bine ai venit la Delgaz!</h1>
       </Container>
     </main>
     <Footer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter,
+createRoutesFromElements,
+Route,
+RouteProvider} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '.assets/styles/index.css';
+import './assets/styles/index.css';
 import App from './App';
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* <Route path="/" element={<HomeScreen />}></Route> */}
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
